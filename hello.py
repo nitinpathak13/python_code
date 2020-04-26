ecd1f1e7be37ebd94480b8261127cfdcf2c281a7
nitinpathak13 <itsnitinpathak@gmail.com>
2020-04-25 23:54:00 -0400

    more string functions

--- hello.py
+++ hello.py
@@ -10,3 +10,12 @@ print(my_str[:5]) # this will print upto 5th character of the string
 print(my_str[4:]) # this will print 4th letter onwards of the string
 print(my_str[2:9]) # this will print 2nd to 8th letter of the string
 print(my_str[2:9:3]) #this will print every 3rd character between 2md and 9th character
+print(my_str[::-1]) # this will print reverse of the string
+
+# Following are string examples
+
+str_1='noopur'
+print(str_1[3:])
+str_1='pur' + str_1[3:]
+print(str_1)
+print(str_1.split('r'))
