8013b2c383356862c15c35e16fb0b856aabfaab3
nitinpathak13 <itsnitinpathak@gmail.com>
2020-04-26 12:20:28 -0400

    more concepts

---
+++ 2ndsteps.py
@@ -0,0 +1,10 @@
+# We will start with lists
+
+list_1=['alpha','beta','gamma']
+list_1.append('delta')
+list_1.sort()
+print(list_1)
+
+# this is for dictionaries
+
+dict_1 = {'xbox':'229','ps4':'199'}
