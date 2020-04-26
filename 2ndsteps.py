abf6dd62b3d55ac4a7a7721458459729a5b9ec12
nitinpathak13 <itsnitinpathak@gmail.com>
2020-04-26 17:34:25 -0400

    instructions about file functions

--- 2ndsteps.py
+++ 2ndsteps.py
@@ -8,3 +8,18 @@ print(list_1)
 # this is for dictionaries
 
 dict_1 = {'xbox':'229','ps4':'199'}
+print(dict_1['xbox'])
+print(dict_1.keys())
+print(dict_1.values())
+
+# tuples
+
+tup1=(12,34,56)
+print ('\n')
+print(tup1[0])
+
+# files
+myfile=open('myfile.txt')
+print(myfile.readlines())
+myfile.seek(0)
+myfile.close()
