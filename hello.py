e341be26e28916565ab5e9cfb700ab444bf347ad
nitinpathak13 <itsnitinpathak@gmail.com>
2020-04-25 23:10:42 -0400

    basics of string

---
+++ hello.py
@@ -0,0 +1,12 @@
+a=100
+b=233
+print(a+b)
+my_str='hello world'
+print(my_str)
+
+# Following are example of slicing
+print(my_str[3]) # this will print character number 3 of string
+print(my_str[:5]) # this will print upto 5th character of the string
+print(my_str[4:]) # this will print 4th letter onwards of the string
+print(my_str[2:9]) # this will print 2nd to 8th letter of the string
+print(my_str[2:9:3]) #this will print every 3rd character between 2md and 9th character
