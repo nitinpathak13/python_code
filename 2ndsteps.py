291536cc1c785a765a224b81365b96880120a1dd
nitinpathak13 <itsnitinpathak@gmail.com>
2020-04-26 23:08:29 -0400

    Concepts of for loop

--- 2ndsteps.py
+++ 2ndsteps.py
@@ -19,7 +19,30 @@ print ('\n')
 print(tup1[0])
 
 # files
-myfile=open('myfile.txt')
-print(myfile.readlines())
+myfile=open('examples.txt')
+# print(myfile.readlines())
 myfile.seek(0)
 myfile.close()
+
+
+# Statements
+
+if True:
+    print('Its True\n')
+
+list_sum=0
+list_2=[1,2,3,4,5]
+
+for i in list_2:
+    list_sum += i
+    print(list_sum)
+
+# Important, use of for in an unassigned counter
+# We use _
+for _ in 'Noopur Mishra':
+    print('Hot!!')
+
+# tuple unpacking
+list_3=[(1,2),(3,4),(5,6),(7,8)]
+for a,b in list_3:
+    print(f'{a} is a pair of {b}')
